604f5e6a6e875e0f8a91585c5d61b90e1b290ce1
swizz <emailswizz@gmail.com>
2024-01-04 04:50:12 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add more paths to any other file that may contain Tailwind CSS classes
+  ],
+  // Add any other custom configurations here
+};
